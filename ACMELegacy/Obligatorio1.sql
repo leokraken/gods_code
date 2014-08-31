7cc2b6114e5c38c80d995f54304d003e3ddad1c2
Cristiano Coelho <cristianocca@hotmail.com>
2014-08-31 19:35:23 -0300

    cambio en script sql segun v2 subida...

--- ACMELegacy/Obligatorio1.sql
+++ ACMELegacy/Obligatorio1.sql
@@ -3,13 +3,13 @@ CREATE DATABASE IF NOT EXISTS middleware;
 
 CREATE TABLE middleware.transactions
 (
-	id					BIGINT NOT NULL PRIMARY KEY,
-	datetime 			DATETIME NOT NULL,
-	transaction_type	CHAR(1) NOT NULL,
-	commerce_code		VARCHAR(20) NOT NULL,
-	commerce_name		VARCHAR(20) NOT NULL,
-	card_number			VARCHAR(20) NOT NULL,
-	card_type			CHAR(1) NOT NULL,
-	currency_code		CHAR(3) NOT NULL,
-	amount				NUMERIC(15,2) NOT NULL
+	id			BIGINT(20) UNSIGNED,
+	datetime 		DATETIME,
+	transaction_type	CHAR(1),
+	commerce_code		VARCHAR(20),
+	commerce_name		VARCHAR(20),
+	card_number		VARCHAR(20),
+	card_type		CHAR(1),
+	currency_code		CHAR(3),
+	amount			NUMERIC(15,2)
 );
