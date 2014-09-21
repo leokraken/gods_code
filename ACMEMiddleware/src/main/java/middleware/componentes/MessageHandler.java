4c025e276454bb369095b6096f66519e8bf744c9
Fede <Fede@Fede-Pc>
2014-09-21 16:57:03 -0300

    Logger con log4j agregado.

--- ACMEMiddleware/src/main/java/middleware/componentes/MessageHandler.java
+++ ACMEMiddleware/src/main/java/middleware/componentes/MessageHandler.java
@@ -5,6 +5,8 @@ import org.springframework.messaging.support.ErrorMessage;
 import middleware.clases.datatypes.Result;
 import middleware.clases.datatypes.TransactionStatus;
 
+import org.apache.log4j.Logger;
+
 /***
  * Clase de ejemplo para recibir y loguear mensajes invalidos.
  * El invalid message channel es publish suscribe, por lo que se podrian recibir los mensajes de muchas formas.
@@ -14,24 +16,29 @@ import middleware.clases.datatypes.TransactionStatus;
 
 public class MessageHandler {
 	
-		
+	final static Logger logger = Logger.getLogger(MessageHandler.class.getName());
+	
 	public void LogInvalid(TransactionStatus t){
-		System.out.println("Mensaje invalido recibido por middleware: " + t.getMessage());		
+		logger.error("Mensaje invalido recibido por middleware: " + t.getMessage());
+		//System.out.println("Mensaje invalido recibido por middleware: " + t.getMessage());		
 	}
 	
 	public void LogError(ErrorMessage e){
 		if (e.getPayload().getCause() != null){
-			System.out.println("Excepcion: " + e.getPayload().getCause());
+			logger.error("Excepcion: " + e.getPayload().getCause());
+			//System.out.println("Excepcion: " + e.getPayload().getCause());
 		}
 		else{
-			System.out.println("Excepcion: " + e.getPayload().getMessage());
+			logger.error("Excepcion: " + e.getPayload().getMessage());
+			//System.out.println("Excepcion: " + e.getPayload().getMessage());
 		}
 				
 	}
 		
 	public void LogInvalidMessageWS(Result res){
 		if(!res.isOk()){
-			System.out.println(res.getMessage());
+			logger.error("Mensaje invalido desde el WS" + res.getMessage());
+			//System.out.println(res.getMessage());
 		}
 	}
 
